43d81244220dc5892d9e965a4b5f385b9cdf20c7
abhinav0111 <121910302017@gitam.in>
2024-06-03 13:41:14 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
-import os
-
+from flask import Flask
 # Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    value = os.getenv('MY_VALUE')
-    return f'Hello, World! from {value}'
-
-@app.route('/value')
-def get_value():
-    value = os.getenv('MY_VALUE')
-    return render_template('value.html', value=value)
+    return f'Hello, World! Application is running successfully.'
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
