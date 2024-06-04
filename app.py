42b72fedff97e81714d76fb3ec84a15519e5c9fd
abhinav0111 <121910302017@gitam.in>
2024-06-04 15:12:14 +0530

    Add: 2nd committt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f'Hello, World! Application is running successfully.'
+    return f'Hello, World!!! Application is running successfully.'
 
 if __name__ == '__main__':
     app.run(port=5000)
