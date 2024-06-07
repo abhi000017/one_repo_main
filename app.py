ee805e613a1286cd733fa8d3136f891192549a2a
abhinav0111 <121910302017@gitam.in>
2024-06-07 15:04:41 +0530

    html eidt

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f'Hello, World!!! Application is running successfully.'
+    return '''
+    <html>
+        <body style="background-color: powderblue;">
+            <h1 style="text-align: center; font-size: 2em; font-family: Arial, sans-serif;">
+                Hello, World!!! Application is running successfully.
+            </h1>
+        </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(port=5000)
